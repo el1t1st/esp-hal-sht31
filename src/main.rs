7040fbc2895bccdfb987087544894a66d7540aa0
Alex K <kleydints@petalmail.com>
2024-06-15 06:47:39 +0300

    activated sht31 and led on measurement

--- src/main.rs
+++ src/main.rs
@@ -3,74 +3,64 @@
 
 use esp_backtrace as _;
 use esp_hal::{
-    clock::ClockControl,
-    delay::Delay,
-    gpio::{Input, Io, Level, Output, Pull},
-    i2c::I2C,
-    peripherals::Peripherals,
-    prelude::*,
-    system::SystemControl,
+    clock::ClockControl, delay::Delay, gpio::IO, i2c::I2C, peripherals::Peripherals,
+    prelude::_fugit_ExtU32, prelude::*,
 };
-use sht31::{mode::Sht31Reader, Accuracy, TemperatureUnit, SHT31};
+use sht31::{prelude::*, Accuracy, TemperatureUnit, SHT31};
+// use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
 
 #[entry]
 fn main() -> ! {
     let peripherals = Peripherals::take();
-    let system = SystemControl::new(peripherals.SYSTEM);
-
+    let system = peripherals.SYSTEM.split();
     let clocks = ClockControl::max(system.clock_control).freeze();
 
     esp_println::logger::init_logger_from_env();
 
     // 1. create an io instance
-    let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
 
     // 2. Create an instance of a led on a gpio port
-    let mut led = Output::new(io.pins.gpio7, Level::Low);
+    let mut led = io.pins.gpio7.into_push_pull_output();
     led.set_low();
 
-    // 3. Create an instance of a pushbutton on gpio port io5
-    // and set the button high (low means pressed)
-    let button = Input::new(io.pins.gpio20, Pull::Up);
-
+    // let delay = DelayMs::new(&clocks);
     let delay = Delay::new(&clocks);
 
     // 4. Get data from the SHT31 sensor I2C
     // SCL gpio37 SDL clock
     // SDA gpio38 SDA data
-    let i2c = I2C::new(
+    let i2c_sht = I2C::new(
         peripherals.I2C0,
         io.pins.gpio38,
         io.pins.gpio37,
-        100.kHz(),
+        100_u32.kHz(),
         &clocks,
-        None,
     );
-    let mut sht = SHT31::new(i2c, delay)
+
+    let mut sht = SHT31::new(i2c_sht, delay)
         .with_accuracy(Accuracy::High)
         .with_unit(TemperatureUnit::Celsius);
 
-    // 5. Instantiate the oled ssd1315
+    // 5. Instantiate the oled ssd1306
     // create an i2c instance
-    // let (scl, sda) = (io.pins.gpio37, io.pins.gpio38);
+    // sda gpio8 / sdc gpio9
+    // let sda = io.pins.gpio8;
+    // let scl = io.pins.gpio9;
     //
+    // let i2c_oled = I2C::new(peripherals.I2C1, sda, scl, 100_u32.kHz(), &clocks);
+    //
+    // let interface = I2CDisplayInterface::new(i2c_oled);
 
     loop {
-        // log::info!("Privet Mir!");
+        // toggle the led every 2 secs and do a reading
         // led.toggle();
-        // delay.delay(500.millis());
-        if button.is_high() {
-            led.set_low();
-        } else {
-            log::info!("Button clicked!");
-            led.set_high();
-        }
-
+        delay.delay_micros(1_000_000);
+        led.set_high();
         // print out the values from the SHT32 sensor
         let reading = sht.read();
         log::info!("sht: {:?}", reading);
-
-        // delay
-        delay.delay(1000.millis());
+        delay.delay_micros(1_000_000);
+        led.set_low();
     }
 }
