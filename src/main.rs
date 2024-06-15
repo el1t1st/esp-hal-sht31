512c090467daec649d595e8334c3547e88940003
Alex K <kleydints@petalmail.com>
2024-06-15 09:25:09 +0300

    pushed sht31 values in stringformat onto the ssd1306 oled

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 #![no_std]
 #![no_main]
 
-use core::fmt::Write;
 use embedded_graphics::{
-    mono_font::{ascii::FONT_6X10, MonoTextStyle},
+    mono_font::{iso_8859_1::FONT_10X20, MonoTextStyle},
     pixelcolor::BinaryColor,
     prelude::*,
     text::{Text, TextStyle},
@@ -13,8 +12,9 @@ use esp_hal::{
     clock::ClockControl, delay::Delay, gpio::IO, i2c::I2C, peripherals::Peripherals,
     prelude::_fugit_ExtU32, prelude::*,
 };
+use nstr::ToString;
 use sht31::{prelude::*, Accuracy, TemperatureUnit, SHT31};
-use ssd1306::{mode::TerminalMode, prelude::*, I2CDisplayInterface, Ssd1306};
+use ssd1306::{mode::BufferedGraphicsMode, prelude::*, I2CDisplayInterface, Ssd1306};
 
 #[entry]
 fn main() -> ! {
@@ -56,30 +56,49 @@ fn main() -> ! {
     let scl = io.pins.gpio9;
     //
     let i2c_oled = I2C::new(peripherals.I2C1, sda, scl, 100_u32.kHz(), &clocks);
-    //
     let interface = I2CDisplayInterface::new(i2c_oled);
-    let mut display =
-        Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0).into_terminal_mode();
-
+    let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+        .into_buffered_graphics_mode();
     display.init().unwrap();
-    display.clear().unwrap();
 
-    // let text_style = MonoTextStyle::new(&FONT_6X10, BinaryColor::On);
+    let text_style = MonoTextStyle::new(&FONT_10X20, BinaryColor::On);
 
-    // Text::new("Nachinaem izmereniya...", Point::new(10, 10), text_style)
-    //     .draw(&mut display)
-    //     .unwrap();
+    Text::new("Starting measurements ... ", Point::new(10, 10), text_style)
+        .draw(&mut display)
+        .unwrap();
+
+    display.flush().unwrap();
 
     loop {
         // toggle the led every 2 secs and do a reading
         // led.toggle();
-        delay.delay_micros(1_000_000);
+        delay.delay_micros(5_000_000);
+        display.clear_buffer();
+        display.flush().unwrap();
         let _ = led.set_high();
         // print out the values from the SHT32 sensor
-        let reading = sht.read();
+        let reading = sht.read().unwrap();
+        let temperature = reading.temperature.to_string::<10>();
+        let humidity = reading.humidity.to_string::<10>();
 
-        log::info!("sht: {:?}", &reading);
-        delay.delay_micros(1_000_000);
+        Text::new(&temperature, Point::new(10, 30), text_style)
+            .draw(&mut display)
+            .unwrap();
+
+        Text::new(&humidity, Point::new(10, 50), text_style)
+            .draw(&mut display)
+            .unwrap();
+
+        display.flush().unwrap();
+        // match reading {
+        //     Ok(value) => log::info!(
+        //         "temperature: {} humidity: {}",
+        //         value.temperature,
+        //         value.humidity
+        //     ),
+        //     Err(_) => log::info!("no value"),
+        // }
+        delay.delay_micros(5_000_000);
         // Write to led
         let _ = led.set_low();
     }
